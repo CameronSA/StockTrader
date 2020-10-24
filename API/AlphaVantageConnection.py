01eb24403869301aaeedd45f01bf8bfb8008b0c4
cameron simpson-allsop <cammeronallsop95@hotmail.co.uk>
2020-10-24 15:05:35 +0100

    Initial commit

---
+++ API/AlphaVantageConnection.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+
+class StockTimeSeries:
+    def __init__(self):
+        self.__api_key = 'ZNQ9U6GP44W5YTBK'
+
+    def time_series_intraday(self):
+        pass
+
+    def time_series_intraday_extended(self):
+        pass
+
+    def time_series_daily(self):
+        pass
+
+    def time_series_daily_adjusted(self):
+        pass
+
+    def time_series_weekly(self):
+        pass
+
+    def time_series_weekly_adjusted(self):
+        pass
+
+    def time_series_monthly(self):
+        pass
+
+    def time_series_monthly_adjusted(self):
+        pass
+
+    def quote_endpoint(self):
+        pass
+
+    def search_endpoint(self):
+        pass
---
+++ main.py
@@ -0,0 +1,9 @@
+import API.AlphaVantageConnection as data
+
+
+def main():
+    time_series = data.StockTimeSeries
+
+
+if __name__ == '__main__':
+    main()
