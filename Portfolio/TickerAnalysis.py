7a2e2327b6000a7ebb7525650ad108242aaf8762
cameron simpson-allsop <cammeronallsop95@hotmail.co.uk>
2020-12-06 18:06:19 +0000

    Replaced trend analysis with least squares fit of the MA instead of derivative of the MA.
    Added function to rank stocks based on the gradient of the least squares fit. Using Monte Carlo analysis, the ranking is done N times with gradient values being taken as a random value from a normal distribution (mean = gradient, stddev = gradient error). The higher the gradient and the better the fit, the lower the ranking of the stock

--- Portfolio/TickerAnalysis.py
+++ Portfolio/TickerAnalysis.py
@@ -1,10 +1,11 @@
 import API.YahooFinance as yahoo
 import pandas as pd
 import numpy as np
-from scipy.stats import skew, skewtest
+from scipy.optimize import curve_fit
 import pandas as pd
-from numpy import diff
 from tqdm import tqdm
+from datetime import datetime
+import time
 
 
 class TickerAnalysis:
@@ -12,49 +13,116 @@ class TickerAnalysis:
         self.__interval = interval
         self.__time_period = time_period
 
+    def trend_analysis(self, stock_exchange, ma_period):
+        print(f'Performing trend analysis for {stock_exchange}. . .')
+        time.sleep(0.05)
+        self.__set_stock_exchange(stock_exchange)
+        trend_derivative_analysis_dict = {}
+        trend_gradient_analysis_dict = {}
+        # count = 0
+        for ticker in tqdm(self.__stock_listings['Symbol']):
+            # if count >= 20:
+            #     break
+            self.__trend_gradient_analysis(trend_gradient_analysis_dict, ticker, ma_period)
+            # count += 1
+        return trend_gradient_analysis_dict
+
+    def rank_by_trend(self, trend_gradient_analysis_dict, n_trials):
+        tickers = []
+        trend_gradients = []
+        trend_gradient_errors = []
+        for ticker in list(trend_gradient_analysis_dict.keys()):
+            gradient_analysis = trend_gradient_analysis_dict[ticker]
+            popt = gradient_analysis[0]
+            pcov = gradient_analysis[1]
+            popt_error = np.sqrt(np.diag(pcov))
+
+            trend_gradient = popt[0]
+            trend_gradient_error = popt_error[0]
+            tickers.append(ticker)
+            trend_gradients.append(trend_gradient)
+            trend_gradient_errors.append(trend_gradient_error)
+
+        ticker_trends_df = pd.DataFrame({'Key': tickers,
+                                      'Trend Gradient': trend_gradients,
+                                      'Trend Gradient Error': trend_gradient_errors})
+
+        # To take account of errors: rank tickers by gradient where each gradient is taken from a normal distribution
+        # where the mean is the gradient and the standard deviation is the error on the gradient. Repeat this N times.
+        # For each trial, note the position of each ticker.
+        # The ticker's final ranking is the average of all of it's positions rounded to the nearest int. Duplicate
+        # rankings are put in a random order
+        ticker_average_rankings_dict = self.__perform_monte_carlo_ranking(ticker_trends_df, n_trials)
+        ranked_ticker_tuples = sorted(ticker_average_rankings_dict.items(), key=lambda x: x[1], reverse=False)
+        ranked_tickers = []
+        for tpl in ranked_ticker_tuples:
+            ranked_tickers.append(tpl[0])
+        return ranked_tickers
+
+    def __perform_monte_carlo_ranking(self, ticker_trends_df, n_trials):
+        def __perform_unit_simulation(ticker_trends_df):
+            simulated_df = ticker_trends_df
+            simulated_fit_values = []
+            for index, row in simulated_df.iterrows():
+                simulated_fit_values.append(np.random.normal(row['Trend Gradient'], row['Trend Gradient Error']))
+
+            simulated_df['Simulated Gradient'] = simulated_fit_values
+            simulated_df.sort_values('Simulated Gradient', ascending=False, inplace=True)
+            simulated_df.reset_index(drop=True, inplace=True)
+            simulated_df['Ranking'] = simulated_df.index
+            simulated_dict = {}
+            for index, row in simulated_df.iterrows():
+                simulated_dict[row['Key']] = row['Ranking']
+            return simulated_dict
+
+        rankings_list_dict = {}
+        print('Performing Monte Carlo Ranking Simulation. . .')
+        time.sleep(0.05)
+        for i in tqdm(range(0, n_trials)):
+            ticker_rankings_dict = __perform_unit_simulation(ticker_trends_df)
+            for ticker, ranking in ticker_rankings_dict.items():
+                if ticker in rankings_list_dict:
+                    rankings_list_dict[ticker].append(ranking)
+                else:
+                    rankings_list_dict[ticker] = [ranking]
+
+        average_rankings_dict = {}
+        for ticker, rankings_list in rankings_list_dict.items():
+            average_ranking = float(np.mean(rankings_list))
+            average_rankings_dict[ticker] = int(round(average_ranking))
+
+        return average_rankings_dict
+
     def __set_stock_exchange(self, stock_exchange):
         if stock_exchange == 'snp500':
             self.__stock_listings = pd.read_csv('StockListings/SNP500_Listings_2020-11-22.csv')
 
-    def calculate_returns(self, ticker):
-        ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, self.__time_period, self.__interval)
-        returns_data = ohlc_data['Close'].pct_change().dropna()
-        returns = {
-            'returns_data': returns_data,
-            'returns_mean': np.mean(returns_data),
-            'returns_stddev': np.std(returns_data),
-            'returns_median': np.quantile(returns_data, 0.5),
-            'returns_skewness': skew(returns_data),
-            'returns_skewtest': skewtest(returns_data)}
+    def __straight_line(self, x, m, c):
+        return m*x + c
 
-        return returns
-
-    def trend_analysis(self, stock_exchange, ma_period):
-        print('Performing trend analysis for SNP500. . .')
-        self.__set_stock_exchange(stock_exchange)
-        trend_analysis_dict = {}
-        for ticker in tqdm(self.__stock_listings['Symbol']):
-            ohlc_data = pd.DataFrame()
+    def __trend_gradient_analysis(self, trend_analysis_dict, ticker, ma_period):
+        ohlc_data = pd.DataFrame()
+        try:
+            ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, self.__time_period, self.__interval)
+        except Exception as e:
+            print(f'Error encountered for ticker: {ticker}: ', e)
+        if not ohlc_data.empty:
             analysed_data = pd.DataFrame()
+            analysed_data['MA'] = ohlc_data['Close'].rolling(window=ma_period).mean().dropna()
+            analysed_data['UTC'] = analysed_data.index
+
+            def utc_time_difference(time):
+                start = datetime(1970, 1, 1, 12, 0, 0, 0)
+                elapsed_time = time-start
+                return elapsed_time.total_seconds()
+
+            analysed_data['UTC'] = analysed_data['UTC'].apply(lambda x: utc_time_difference(x))
             try:
-                ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, self.__time_period, self.__interval)
-            except Exception as e:
-                print(f'Error encountered for ticker: {ticker}: ', e)
-            if not ohlc_data.empty:
-                analysed_data = pd.DataFrame()
-                analysed_data['MA'] = ohlc_data['Close'].rolling(window=ma_period).mean()
-                derivative = list(diff(analysed_data['MA'])/diff(ohlc_data.index.astype(int)*(10**-9)))
-                derivative.insert(0, np.nan)
-                analysed_data['dMA'] = derivative
-                analysed_data['dMA_MA'] = analysed_data['dMA'].rolling(window=ma_period).mean()
+                popt, pcov = curve_fit(self.__straight_line, analysed_data.UTC, analysed_data.MA)
+                results = (popt, pcov)
                 if ticker not in trend_analysis_dict.keys():
-                    trend_analysis_dict[ticker] = analysed_data
+                    trend_analysis_dict[ticker] = results
                 else:
                     print(f'WARNING: Duplicate tickers found: {ticker}')
-            break
-        return trend_analysis_dict
-
-    @staticmethod
-    def select_from_trend_analysis(trend_analysis_dict):
-        for key, value in trend_analysis_dict.items():
-            break
+            except Exception as e:
+                print(f'Error encountered for ticker {ticker}', e)
--- main.py
+++ main.py
@@ -9,18 +9,23 @@ from Portfolio.TickerAnalysis import TickerAnalysis
 import Visualisation.Plotting as plotting
 
 def main():
-    ticker = 'MMM'
-    ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, '2y', '1d')
-    apply.apply_indicators(ohlc_data)
-    close_data = ohlc_data.drop(columns=['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'])
-    tckr = TickerAnalysis('20y', '1d')
-    trend_analysis_dict = tckr.trend_analysis('snp500', ma_period=100)
-    plotting.Derivative(trend_analysis_dict[ticker], 'MA', 'dMA', 'dMA_MA', f'{ticker} Moving Average Derivative')
+    #ticker = 'MMM'
+    #ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, '2y', '1d')
+    #apply.apply_indicators(ohlc_data)
+    #close_data = ohlc_data.drop(columns=['Open', 'High', 'Low', 'Volume', 'Dividends', 'Stock Splits'])
 
-    plotting.Histogram(trend_analysis_dict[ticker]['MA'], f'{ticker} Moving Average Distribution')
-    plotting.Histogram(trend_analysis_dict[ticker]['dMA'], f'{ticker} Moving Average Time Derivative Distribution')
-    plotting.Histogram(
-        trend_analysis_dict[ticker]['dMA_MA'], f'{ticker} Moving Average Time Derivative Distribution Moving Average')
+    tckr = TickerAnalysis('5y', '1d')
+    trend_gradient_analysis_dict = tckr.trend_analysis('snp500', ma_period=100)
+    ranked_tickers = tckr.rank_by_trend(trend_gradient_analysis_dict, 1000)
+
+    count = 0
+    for ticker in ranked_tickers:
+        if count % 20 == 0:
+            ohlc_data = yahoo.StockTimeSeries.historical_data(ticker, '5y', '1d')
+            ts_plot = plotting.TimeSeriesPlot()
+            ts_plot.add_series(ohlc_data, 'Close', 'Close Price', 'blue')
+            ts_plot.draw('Price ($)', ticker)
+        count+=1
 
     # returns = tckr.calculate_returns()
     # print(returns)
